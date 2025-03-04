5e314b3599adb69be5e3d5246b97fb9f78f0ce6f
Karam Sawalha <89216965+ksawalha@users.noreply.github.com>
2025-03-04 20:01:01 +0300

    Create RareloopAppVersion.m

---
+++ src/ios/RareloopAppVersion.m
@@ -0,0 +1,29 @@
+#import "RareloopAppVersion.h"
+@import Foundation;
+
+@implementation RareloopAppVersion
+
+- (void)getAppVersion:(CDVInvokedUrlCommand*)command {
+    NSDictionary* infoDict = [NSBundle mainBundle].infoDictionary;
+    
+    NSString* appVersion = infoDict[@"CFBundleShortVersionString"];
+    NSString* buildNumber = infoDict[@"CFBundleVersion"];
+
+    if (!appVersion || !buildNumber) {
+        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                  messageAsString:@"Missing version/build in Info.plist"];
+        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        return;
+    }
+
+    NSDictionary* response = @{
+        @"version": appVersion,
+        @"build": buildNumber
+    };
+
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                                messageAsDictionary:response];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
